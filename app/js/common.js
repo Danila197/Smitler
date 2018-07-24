44c81d4e81ed5d7fd932e126e5ba75faa37a0142
Vitaliy Semenoff <deepy_dee@bk.ru>
2018-07-24 15:16:43 +0300

    Footer, up-button, preloader

--- app/js/common.js
+++ app/js/common.js
@@ -100,6 +100,18 @@ $(function() {
 		}
 	});
 
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > $(this).height()) {
+			$('.top').addClass('active');
+		} else {
+			$('.top').removeClass('active');
+		}
+	});
+
+	$('.top').click(function() {
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	});
+
 		//E-mail Ajax Send
 		$("form.callback").submit(function() { //Change
 			var th = $(this);
@@ -125,3 +137,7 @@ $(function() {
 	window.onresize = function() {onResize()};
 
 });
+
+$(window).on('load', function() {
+	$('.preloader').delay(1000).fadeOut('slow');
+})
